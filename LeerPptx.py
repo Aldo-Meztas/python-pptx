165953c75db670545258daa924c5566bd867d3a5
= <=>
2020-10-13 10:57:06 -0500

    cambiosFotmato

--- LeerPptx.py
+++ LeerPptx.py
@@ -6,22 +6,88 @@ Created on Mon Oct 12 15:54:20 2020
 @author: ssccdmx-amh
 """
 from pptx import Presentation
-# para imagenegenes se utiliza este modulo
+# para imagenes se utiliza este modulo
 from pptx.util import Inches, Pt
 from pptx.enum.shapes import MSO_SHAPE
 
 class ArchivosPptx():    
-    def titulo(self,prst,titulo):
-        prs = prst
+    def __init__(self,pathTemplete):
+        self.f = open(pathTemplete)
+        self.prs =Presentation(pathTemplete)        
+        
+    def presentacion(self,titulo):
+        prs = self.prs
         tituDiapo = prs.slide_layouts[0]       
         slide = prs.slides.add_slide(tituDiapo)        
         title = slide.shapes.title             
         title.text = titulo
-         
         
-    def segundoFormato(self, prst):
-        prs = prst
-        bullet_slide_layout = prs.slide_layouts[1]
+    def temaDiapo(self,titulo,subtitulo):        
+        prs = self.prs
+        SLD_LAYOUT_TITLE_AND_CONTENT = 1        
+        slide_layout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
+        slide = prs.slides.add_slide(slide_layout)
+        title = slide.shapes.title        
+        title.text = titulo   
+        subtitle = slide.placeholders[1]
+        subtitle.text = subtitulo
+        
+    def diseñoDiapo1(self,titulo,parrafo,pathImg):
+        prs = self.prs
+        blank_slide_layout = prs.slide_layouts[6]
+        slide = prs.slides.add_slide(blank_slide_layout)        
+        left = top = width = height = Inches(1)
+        
+        title = slide.shapes.title        
+        title.text = titulo
+        
+        txBox = slide.shapes.add_textbox(Inches(4.07), Inches(1.93), Inches(1.82), Inches(5.54))        
+        tf = txBox.text_frame                
+        tf.text = parrafo
+        p = tf.add_paragraph()
+        img_path = pathImg
+        left = Inches(1.00)
+        top = Inches(1.67)
+        height = Inches(2.45)
+        pic = slide.shapes.add_picture(img_path, left, top, height=height)
+    
+    def diseñoDiapo2(self,titulo,parrafo):
+        prs = self.prs
+        blank_slide_layout = prs.slide_layouts[6]
+        slide = prs.slides.add_slide(blank_slide_layout)        
+        
+        
+        title = slide.shapes.title        
+        title.text = titulo
+        
+        txBox = slide.shapes.add_textbox(Inches(1.00), Inches(1.33), Inches(0.57), Inches(8.30))        
+        tf = txBox.text_frame                
+        tf.text = parrafo
+        p = tf.add_paragraph()
+    
+    
+    def agregarImagen(self,pathImg,descripcion):
+        # Ruta de imagen
+        img_path = pathImg
+        prs = self.prs
+        blank_slide_layout = prs.slide_layouts[6]
+        slide = prs.slides.add_slide(blank_slide_layout)
+        
+        left = Inches(5)
+        height = Inches(3.12)
+        
+        pic = slide.shapes.add_picture(img_path, Inches(3.14), Inches(0.60), height=height)
+        
+        # Descripcion de imagen
+        txBox = slide.shapes.add_textbox(Inches(1.81), Inches(4.05), Inches(0.81), Inches(5.94))        
+        tf = txBox.text_frame                
+        tf.text = descripcion
+        p = tf.add_paragraph()
+    
+    def diapoViñetas(self,titulo,tema,subtema,puntos):
+        prs = self.prs
+        
+        bullet_slide_layout = prs.slide_layouts[3]
         
         slide = prs.slides.add_slide(bullet_slide_layout)
         shapes = slide.shapes
@@ -29,39 +95,25 @@ class ArchivosPptx():
         title_shape = shapes.title
         body_shape = shapes.placeholders[1]
         
-        title_shape.text = 'Diapositiva de viñeta'
+        title_shape.text = titulo
         
         tf = body_shape.text_frame
-        tf.text = 'Find the bullet slide layout'
+        tf.text = tema
         
         p = tf.add_paragraph()
-        p.text = 'Use _TextFrame.text for first bullet'
+        p.text = subtema
         p.level = 1
         
         p = tf.add_paragraph()
-        p.text = 'Use _TextFrame.add_paragraph() for subsequent bullets'
-        p.level = 2
+        p.text = puntos
+        p.level = 1
         
-    def agregarImagen(self,templete,pathImg):
-        # Ruta de imagen
-        img_path = pathImg
-
-        prs = Presentation(templete)
-        blank_slide_layout = prs.slide_layouts[6]
-        slide = prs.slides.add_slide(blank_slide_layout)
-        
-        left = top = Inches(1)
-        pic = slide.shapes.add_picture(img_path, left, top)
-        
-        left = Inches(5)
-        height = Inches(5.5)
-        pic = slide.shapes.add_picture(img_path, left, top, height=height)
-        prs.save('Salida.pptx')
+    
         
     def crearTabla(self):
         
         from pptx.util import Inches
-        prs = Presentation()
+        prs = self.prs
         title_only_slide_layout = prs.slide_layouts[5]
         slide = prs.slides.add_slide(title_only_slide_layout)
         shapes = slide.shapes
@@ -87,86 +139,28 @@ class ArchivosPptx():
         table.cell(1, 0).text = 'valo-1'
         table.cell(1, 1).text = 'valor-2'
         
-        prs.save('creacionTabla.pptx')
-        
-    def diseñoDiapo1(self,prs):        
-        SLD_LAYOUT_TITLE_AND_CONTENT = 1        
-        slide_layout = prs.slide_layouts[SLD_LAYOUT_TITLE_AND_CONTENT]
-        slide = prs.slides.add_slide(slide_layout)
-        title = slide.shapes.title        
-        title.text = "Diapositiva"        
-        subtitle = slide.placeholders[1]
-        subtitle.text = "subtitulo!"
-        
-    def diseñoDiapo2(self,prs):
-        # =====================================
-        blank_slide_layout = prs.slide_layouts[6]
-        slide = prs.slides.add_slide(blank_slide_layout)        
-        left = top = width = height = Inches(1)
-        
-        title = slide.shapes.title        
-        title.text = "Diapositiva"
-        
-        txBox = slide.shapes.add_textbox(Inches(4.07), Inches(1.93), Inches(1.82), Inches(5.54))        
-        tf = txBox.text_frame                
-        tf.text = "Esto es un parrafo de texto \n"+"Hola mundo"        
-        p = tf.add_paragraph()
-        # p.text = "This is a second paragraph that's bold"
-        # p.font.bold = True
-        # p = tf.add_paragraph()
-        # p.text = "This is a third paragraph that's big"
-        # p.font.size = Pt(30)
-        #========================================
-        img_path = 'descarga.png'             
-        left = Inches(1.00)
-        top = Inches(1.67)
-        height = Inches(2.45)
-        pic = slide.shapes.add_picture(img_path, left, top, height=height)
-        # left = Inches(1)
-        # height = Inches(1)
-        # pic = slide.shapes.add_picture(img_path, left, top, height=height) 
-        # left = top = Inches(1)
-        # pic = slide.shapes.add_picture(img_path, left, top)
+    
         
             
         
-    def abrirPlantilla(self,ruta):
-        f = open(ruta)
-        prs = Presentation(ruta)        
-        self.titulo(prs,'Presentacion')                
-                
-        self.diseñoDiapo1(prs)
-        self.diseñoDiapo2(prs)
-        
-        
-# =============================================================================
-#         shapes = slide.shapes
-#         left = top = width = height = Inches(1.0)
-#         shape = shapes.add_shape(
-#             MSO_SHAPE.CUBE, left, top, width, height
-#         )
-# =============================================================================
-        
-# =============================================================================
-#         shape.text = 'foobar'
-# 
-#         # is equivalent to ...
-#         
-#         text_frame = shape.text_frame
-#         text_frame.clear()
-#         p = text_frame.paragraphs[0]
-#         run = p.add_run()
-#         run.text = 'foobar'
-# =============================================================================
+
+
+    def cerrarPlantilla(self):
+        self.prs.save('Salida.pptx')    
+        self.f.close()
 
         
-        
-                
-        prs.save('Salida.pptx')    
-                
-        f.close()
+# =================EJEMPLO PARA DIAPOSITIVAS=================
+miObjeto = ArchivosPptx('templete.pptx')
 
-        
+texto = "Esto es una breve descripcion de la imagen por texto \n"+"hola Mundo"
+img = 'descarga.png'
 
-miObjeto = ArchivosPptx()
-miObjeto.abrirPlantilla('templete.pptx')
+miObjeto.presentacion('presentacion')
+miObjeto.temaDiapo("Diapositiva", "subtitulos")
+miObjeto.diseñoDiapo1('titulo', 'parrafo', img)
+miObjeto.agregarImagen(img,texto)
+miObjeto.diseñoDiapo2('titulo', texto)
+
+miObjeto.cerrarPlantilla()
+# ===================FIN EJEMPLO===================
